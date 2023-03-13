f81af0d1ca7825d91d2e8e1f8ea51745ff2938c1
Avijit Biswas <71291040+avizyt@users.noreply.github.com>
2023-03-14 00:26:41 +0530

    documentation getUserTimeline

--- gathertweets/app.py
+++ gathertweets/app.py
@@ -66,14 +66,50 @@ def save_dict_to_jsonl(dict_to_save, file_path):
         json.dump(dict_to_save, outfile)
         outfile.write('\n')
 
+
+"""
+Function to retrieve the timeline of a Twitter user and save it to a JSON Lines file.
+
+Args:
+    screename (str): The screen name of the Twitter user whose timeline is to be retrieved.
+
+Returns:
+    A list of dictionaries containing the details of each tweet in the user's timeline.
+
+Examples:
+    >>> user_timeline = getUserTimeline('elonmusk')
+"""
+
 def getUserTimeline(screename):
+    """
+    Retrieve the timeline of a Twitter user and save it to a JSON Lines file.
+
+    The function uses the Twitter API to retrieve the most recent 300 tweets from the specified user's timeline,
+    and saves each tweet's details (text, user, id, and created_at) to a JSON Lines file with the user's screen name in the filename.
+
+    Args:
+        screename (str): The screen name of the Twitter user whose timeline is to be retrieved.
+
+    Returns:
+        A list of dictionaries containing the details of each tweet in the user's timeline.
+
+    Raises:
+        tweepy.TweepError: If there is an error connecting to the Twitter API or retrieving the user's timeline.
+        IOError: If there is an error writing to the JSON Lines file.
+    """
+
+    # Retrieve the most recent 300 tweets from the user's timeline.
     tweets = api.user_timeline(screen_name=screename, 
         # 200 is the maximum allowed count
         count=300,
         include_rts = False,
         tweet_mode = 'extended' #for full text
     )
+
+    # Initialize an empty list to store the tweet details.
     dataarray = []
+
+    # Iterate over each tweet and extract its details.
     for tweet in tweets:
         dataset = {}
         dataset['text'] = tweet.full_text
@@ -81,8 +117,14 @@ def getUserTimeline(screename):
         dataset['id'] = tweet.id
         dataset['created_at'] = str(tweet.created_at)
         dataarray.append(dataset)
+
+        # Save the tweet details to a JSON Lines file with the user's screen name in the filename.
         save_dict_to_jsonl(dataset, f'./data_{tweet.user.screen_name}.jsonl')
+    
+    # Print the number of tweets retrieved.
     print(f'Tweets retruned : {str(len(tweets))}')
+
+    # Return the list of tweet details.
     return dataarray
 
 
