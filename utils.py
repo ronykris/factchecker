0ff6b23dfc5f61f3898bb1796b8fa54f54b8fe35
Avijit Biswas <71291040+avizyt@users.noreply.github.com>
2023-03-14 22:09:02 +0530

    modifiying outputpath

--- utils.py
+++ utils.py
@@ -1,4 +1,12 @@
-def jsonl2json(file_path):
+def jsonl2json(file_path, output_path):
+    """Conver Jsonl to json
+    Example:
+        file_path = "./data.jsonl"
+        output_path = "./datasets/newfile.json"
+
+        jsonl2json(file_path, output_path)
+    
+    """
     import pandas as pd
     import json
     output_data = []
@@ -7,4 +15,4 @@ def jsonl2json(file_path):
             line = json.loads(line)
             output_data.append(line)
     data = pd.DataFrame(output_data)
-    return data.to_json("./output.json")
+    return data.to_json(output_path)
